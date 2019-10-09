53f732d3aad79f7b8aa0d4a406d02646e14cbef4
yuanqing <desk@yuanqing.sg>
2019-10-09 20:20:19 +0800

    Set `shell`

--- src/common/execute-bash-commands.js
+++ src/common/execute-bash-commands.js
@@ -3,7 +3,7 @@ import pEachSeries from 'p-each-series'
 
 export function executeBashCommands (bashCommands) {
   return pEachSeries(bashCommands, async function (bashCommand) {
-    const { stdout } = await execa.command(bashCommand)
+    const { stdout } = await execa.command(bashCommand, { shell: true })
     if (stdout.length !== 0) {
       console.log(stdout)
     }
