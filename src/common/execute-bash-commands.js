fd50a0715386f43c3d166f63e912c8925cd17038
Yuan Qing Lim <yuanqing.lim@shopify.com>
2019-10-09 13:54:05 +0800

    Use `execa.command`

--- src/common/execute-bash-commands.js
+++ src/common/execute-bash-commands.js
@@ -1,9 +1,9 @@
-import { shell } from 'execa'
+import execa from 'execa'
 import pEachSeries from 'p-each-series'
 
 export function executeBashCommands (bashCommands) {
   return pEachSeries(bashCommands, async function (bashCommand) {
-    const { stdout } = await shell(bashCommand)
+    const { stdout } = await execa.command(bashCommand)
     if (stdout.length !== 0) {
       console.log(stdout)
     }
